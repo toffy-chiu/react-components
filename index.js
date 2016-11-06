04ba3aca54b50befe1e8fe11f3c3411b712b7fd6
toffy <toffy@toffy-desktop.(none)>
2016-11-06 22:05:37 +0800

    add DateInput

---
+++ index.js
@@ -0,0 +1,10 @@
+var ReactDOM=require('react-dom');
+
+var DateInput = require('./lib/DateInput');
+ReactDOM.render(
+    <div>
+        <DateInput/>
+    </div>
+    ,
+    document.getElementById('container')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,49 @@
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path=require('path');
+
+// 生产环境
+var isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
     entry: {
         index: [
-            'webpack-dev-server/client?http://192.168.1.109:3000',
+            'webpack-dev-server/client?http://0.0.0.0:3000',
             'webpack/hot/only-dev-server',
-            './components/index'
+            './index'
         ]
     },
     output: {
         publicPath:'/',
-        path: path.join(__dirname, 'js'),
-        filename: '[name].js'
+        path: path.resolve(__dirname, './dist'), //js的发布路径
+        filename: '[name].js',
+        chunkFilename:'[name].chunk.js'
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['', '.js']
     },
     module: {
         loaders: [
-            {test: /\.jsx?$/, loader: 'react-hot/webpack!jsx?harmony', exclude:/node_modules/}
+            {test: /\.js$/, loader: 'react-hot/webpack!jsx?harmony', exclude:/node_modules/},
+            {test: /\.css$/, loaders: ['style', 'css'], include:path.join(__dirname, './lib/css')}
         ]
     },
     plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify(isProd?"production":"dev")
+            }
+        }),
+        new webpack.ProvidePlugin({
+            React:'react'
+        }),
         new webpack.optimize.CommonsChunkPlugin('common.js'),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            title:'React Components',
+            template:'./index.html',
+            filename:'./index.html' //结合output.path
+        })
     ]
 };
